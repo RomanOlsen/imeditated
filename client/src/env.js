b6a7aac3682968b4ccf7cd70c98c969895b12ad7
Roman <romanjolsen@gmail.com>
2025-06-06 17:36:41 -0600

    connectedtoAuth0App

--- client/src/env.js
+++ client/src/env.js
@@ -1,6 +1,6 @@
 export const dev = window.location.origin.includes('localhost')
-export const baseURL = dev ? 'http://localhost:3000' : ''
+export const baseURL = dev ? 'http://localhost:3000' : '' // should it be https
 export const useSockets = false
-export const domain = ''
-export const clientId = ''
-export const audience = ''
+export const domain = 'dev-1eliqcykgosr1ydc.us.auth0.com'
+export const clientId = 's0nU8R1ERQStVGR7wgrvGZcdA70d8kyQ'
+export const audience = 'https://auth.imeditated.com'
