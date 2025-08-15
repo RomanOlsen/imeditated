15001927a54d4cd7a43680538a30b3954fcdda2d
Roman <romanjolsen@gmail.com>
2025-08-15 10:57:49 -0600

    Changing the world

--- client/src/pages/AboutPage.vue
+++ client/src/pages/AboutPage.vue
@@ -6,26 +6,33 @@
   <div class="about container mt-3">
     <h1>I meditated.com</h1>
     <p>Built by Roman Olsen</p>
-    <p>I started meditating using apps such as Medito. The guided sessions really grew on me, but as time went on, I noticed a problem. I wanted to keep up my “streak” of meditations in their app, as I found daily practice to be highly beneficial, but I also now wanted the option to meditate differently - whether using a different tool or just not relying on my phone.
+    <p>I started meditating using apps such as Medito. The guided sessions really grew on me, but as time went on, I
+      noticed a problem. I wanted to keep up my “streak” of meditations in their app, as I found daily practice to be
+      highly beneficial, but I also now wanted the option to meditate differently - whether using a different tool or
+      just not relying on my phone.
 
-Most habit trackers I found online are great, but meditation was really the only habit I actually wanted to track daily.
+      Most habit trackers I found online are great, but meditation was really the only habit I actually wanted to track
+      daily.
 
-That’s how I led to imeditated.com. It’s my accountability friend for a habit that truly keeps me grounded. </p>
-    
-    <p>Stay connected to my projects on <a href="https://x.com/RomanOlsenHQ" target="_blank" rel="noopener noreferrer">X</a> or <a href="https://youtube.com/RomanOnFire" target="_blank" rel="noopener noreferrer">YouTube!</a> Reach out if you have any questions or suggestions! (My DMs on X are open)</p>
-      <!-- You can also DM me on <a href="https://instagram.com/romanonfire" target="_blank" rel="noopener noreferrer">Instagram</a> if you have questions. -->
-<p>Changelog from 7/22/25:
-  Added a timer option. Use the timer to have it auto-marked when the time is up.
-</p>
-<p>Changelog from 7/21/25:
-  Added iOS home screen support and changed default duration to 1 minute.
-</p>
+      That’s how I led to imeditated.com. It’s my accountability friend for a habit that truly keeps me grounded. </p>
+
+    <p>Watch the <a href="https://youtu.be/Nd5em108ZXk" target="_blank" rel="noopener noreferrer">video</a></p>
+
+    <p>Follow me on <a href="https://x.com/RomanOlsenHQ" target="_blank" rel="noopener noreferrer">X</a> or <a
+        href="https://youtube.com/RomanOnFire" target="_blank" rel="noopener noreferrer">YouTube!</a> Reach out if you
+      have any questions or suggestions! (My DMs on X are
+      open)</p>
+    <!-- You can also DM me on <a href="https://instagram.com/romanonfire" target="_blank" rel="noopener noreferrer">Instagram</a> if you have questions. -->
+    <p>Changelog from 7/22/25:
+      Added a timer option. Use the timer to have it auto-marked when the time is up.
+    </p>
+    <p>Changelog from 7/21/25:
+      Added iOS home screen support and changed default duration to 1 minute.
+    </p>
 
     <!-- Developers/Employers!
     <a href="https://RomanOlsen.com" target="_blank" rel="noopener noreferrer">Visit my website</a> -->
   </div>
 </template>
 
-<style scoped lang="scss">
-
-</style>
+<style scoped lang="scss"></style>
