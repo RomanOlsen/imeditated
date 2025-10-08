dd2c66c76294f82eac8f22f7dd99ce8bc667209f
Roman <romanjolsen@gmail.com>
2025-10-07 18:22:12 -0600

    updated domain

--- client/src/env.js
+++ client/src/env.js
@@ -1,6 +1,6 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : 'https://imeditated.onrender.com'
 export const useSockets = false
-export const domain = 'auth.imeditated.com'
+export const domain = 'dev-1eliqcykgosr1ydc.us.auth0.com'
 export const clientId = 's0nU8R1ERQStVGR7wgrvGZcdA70d8kyQ'
 export const audience = 'https://auth.imeditated.com'
